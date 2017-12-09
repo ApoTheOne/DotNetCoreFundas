419caf20edd5a5cabacb1f4b8948fd6c9c31082f
ApoTheOne <anurag.saklani4@gmail.com>
2017-12-09 16:37:45 +0000

    Using IApplication Builder and Custom Middleware

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DotNetCoreFunda
 {
@@ -22,12 +23,41 @@ namespace DotNetCoreFunda
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app,
                               IHostingEnvironment env,
-                              IGreeter greeter)
+                              IGreeter greeter,
+                              ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //Following line or method is only called once when application starts
+            app.Use(next => 
             {
-                app.UseDeveloperExceptionPage();
-            }
+                //Following line or method is only called once every request comes
+                return async context =>
+                {
+                    logger.LogInformation("Incoming request!");
+                    if(context.Request.Path.StartsWithSegments("/customMiddlewarePath"))
+                    {
+                        await context.Response.WriteAsync("Responded by our custom middleware!");
+                        logger.LogInformation("Incoming request handled by our middleware!");
+                    }
+                    else
+                    {
+                        /*This will go to next middleware if applcable and execute suitable one.
+                          For eg: in case when url doesnt match with "/customMiddlewarePath" next() middleware will be called 
+                          i.e. app.Run(); as app.UseWelcomePage's path doesn't matches, which Greets with message from config! */
+                        await next(context);
+                        logger.LogInformation("Outgoing response!");
+                    }
+                };
+            });
+
+            app.UseWelcomePage(new WelcomePageOptions()
+            {
+                Path = "/onlyForWelcomePage"
+            });
 
             app.Run(async (context) =>
             {
