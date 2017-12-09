33e4c854c3bef463e3a625c0df34832ba970309f
ApoTheOne <anurag.saklani4@gmail.com>
2017-12-09 08:48:29 +0000

    Implemented Greeter service and injected it in Configure method in StartUp

---
+++ ConfigGreeter.cs
@@ -0,0 +1,10 @@
+namespace DotNetCoreFunda
+{
+    public class ConfigGreeter : IGreeter
+    {
+        public string GetMessage()
+        {
+            return "Hardcoded message!";
+        }
+    }
+}
