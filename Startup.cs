f3898ea5a32b00fcadaf289e494ee74c5f699ddd
ApoTheOne <anurag.saklani4@gmail.com>
2017-12-10 12:15:33 +0530

    Added template and attribute routing

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -18,6 +19,7 @@ namespace DotNetCoreFunda
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, ConfigGreeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,13 +33,33 @@ namespace DotNetCoreFunda
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseFileServer();
+            app.UseStaticFiles();
+
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(ConfigureRoutes);
 
             app.Run(async (context) =>
             {
                 var greetings = greeter.GetMessage();
+                /* If we wont set content type then Not Found is printed without spaces!
+                  context.Response.ContentType = "text/plain";
+                  await context.Response.WriteAsync($"Not Found");
+                */
                 await context.Response.WriteAsync($"{greetings} : {env.EnvironmentName}");
+
             });
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            //No default set in this mapping.
+            //routeBuilder.MapRoute("Default",
+            //    "{controller}/{action}/{id?}");
+            //Now we will set optional params == Defaults so that there is a default route.
+            routeBuilder.MapRoute("Default",
+                "{controller=Home}/{action=Index}/{id?}");
+            routeBuilder.MapRoute("Admin",
+                "admin/{controller}/{action}/{id?}");
         }
     }
 }
